cdc53f9e6df630434c689a72b502474a6f1e1eab
willm625 <wcornej1@binghamton.edu>
2022-11-09 13:41:53 -0500

    account not found

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -173,3 +173,7 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)

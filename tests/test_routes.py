e86ab8114b0733bfec0947c20fd9bc2116897153
willm625 <wcornej1@binghamton.edu>
2022-11-11 15:17:00 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -21,9 +21,15 @@ DATABASE_URI = os.getenv(
 BASE_URL = "/accounts"
 
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
+
+
 ######################################################################
+
+
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -190,6 +196,7 @@ class TestAccountService(TestCase):
 
     def test_cors_security(self):
         """It should return a CORS header"""
+
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
